f1087817d66e76b70d707daae51467c6c043d65f
RajatSharma404 <168256460+RajatSharma404@users.noreply.github.com>
2026-09-05 16:40:02 +0530

    chore(frontend): remove scratch test-next-auth and test.js files

--- frontend/test-next-auth.ts
+++
@@ -1,4 +0,0 @@
-import { authOptions } from "./src/app/api/auth/[...nextauth]/route";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-console.log(authOptions.providers[0].id);
--- frontend/test.js
+++
@@ -1,4 +0,0 @@
-fetch("http://localhost:3005/api/auth/providers")
-  .then(res => res.json())
-  .then(console.log)
-  .catch(console.error)
